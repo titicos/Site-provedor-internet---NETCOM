cd7ca0561b3fd1e6b140c00105ac620b6c6ce198
TIAGO DA SILVA SOUZA <admtiagosouza@gmail.com>
2022-09-08 22:55:18 -0400

    menu mobile ajustado

--- script.js
+++ script.js
@@ -8,4 +8,13 @@ menu.addEventListener("click",function(event){
 event.preventDefault();
 mobile.classList.toggle("display");
 
+if(mobile.classList.contains('display')){
+    document.querySelector(".mobile-imagem").src="imagem/close.png"
+}
+else{
+   
+  document.querySelector(".mobile-imagem").src="imagem/menu-mobile.png"
+    
+}
+
 })

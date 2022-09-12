267f02f496053a2148a5a547b931279b900212b8
TIAGO DA SILVA SOUZA <admtiagosouza@gmail.com>
2022-09-12 14:01:33 -0400

    items ajustados

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let mobile=document.querySelector(".mobile");
 menu.addEventListener("click",function(event){
 event.preventDefault();
 mobile.classList.toggle("display");
+mobile.classList.add("transicao")
 
 if(mobile.classList.contains('display')){
     document.querySelector(".mobile-imagem").src="imagem/close.png"
